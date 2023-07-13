07edf0ad9c6fe917fd1b7ef3e4b6b229fa21307d
hetashridk <hetashrik@gmail.com>
2023-07-13 23:53:07 +0530

    about us section added

--- components/Layout/Aboutus.js
+++ components/Layout/Aboutus.js
@@ -1,12 +1,15 @@
 function Aboutus() {
     return (
         <>
-        <div className="relative top-28 z-10">
-            <p className="text-center text-7xl text-white font-bold">ABOUT US</p>
+        <div className="relative bottom-[39rem] md:bottom-[29rem] lg:bottom-[10rem] z-10 flex flex-col items-center justify-center">
+            <p className="text-white text-center text-2xl font-bold md:text-6xl">ABOUT US</p>
         </div>
-        <div className="absolute top-[36rem] bg-[#0057FF]">
-            <p className="text-white text-center p-32 text-2xl">At Fintract Global Ltd, we combine a cutting-edge tech stack with exceptional talent from Europe, Asia and America to lead change in how financial entities work. Headquartered in London, Fintract Global develops cutting edge fintech and regtech products.
+        <div className="w-full absolute top-[14rem] md:top-[25rem] lg:top-[44rem]">
+        <div className="relative bottom-8 bg-[#0057FF]">
+            <p className="text-white text-center p-9 text-[10px] md:text-2xl md:p-20">At Fintract Global Ltd, we combine a cutting-edge tech stack with exceptional talent from Europe, Asia and America to lead change in how financial entities work. Headquartered in London, Fintract Global develops cutting edge fintech and regtech products.
 </p>
+        </div>
+
         </div>
         </>
     );
