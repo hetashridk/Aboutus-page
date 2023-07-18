03b7222ada1f1abd748438a20bc741a2f652fe24
hetashridk <hetashrik@gmail.com>
2023-07-18 21:42:31 +0530

    Success section added

--- components/Layout/Success.js
+++ components/Layout/Success.js
@@ -1,7 +1,40 @@
 function Success() {
   return (
     <>
-      <div className="relative top-[35rem]">
+
+    <div>
+        <p className="text-center -mt-[30rem] font-semibold md:-mt-16 md:font-bold md:text-2xl lg:mt-44 lg:text-3xl">SUCCESS BY THE NUMBERS</p>
+        <p className="p-5 text-center text-[13px] md:text-xl">Sometimes numbers are the easiest way to get to the truth. Check out
+          these business highlights to learn more and get to know us better</p>
+
+          <div className="flex flex-col m-3  md:flex-row justify-center items-center mt-10">
+              <div className="flex ">
+                <div className="space-x-4">
+                <p className="text-center text-2xl text-[#0057FF] md:text-4xl">20K+</p>
+                <p className="text-center text-[13px] text-[#666666] md:text-[15px]">User already registered and</p>
+                <p className="text-center text-[13px] text-[#666666] md:text-[15px]"> signing up for our product</p>
+                </div>
+                <div className="space-x-4">
+                <p className="text-center text-2xl text-[#0057FF] md:text-4xl">94%</p>
+                <p className="text-center text-[13px] text-[#666666] md:text-[15px]">Daily Activity Engagement</p>
+                </div>
+              </div>
+              <div className="flex md:-mt-10">
+                <div className="space-x-4">
+                <p className="text-center text-2xl text-[#0057FF] mt-10 md:text-4xl">10.2K+</p>
+                <p className="text-center text-[13px] text-[#666666] md:text-[15px]">Happy clients and worke</p>
+                <p className="text-center text-[13px] text-[#666666] md:text-[15px]">done</p>
+                </div>
+                {/* <p className="text-center text-[13px] text-[#666666]"> signing up for our product</p> */}
+                <div className="space-x-4">
+                <p className="text-center text-2xl text-[#0057FF] mt-10 md:text-4xl">95+</p>
+                <p className="text-center text-[13px] text-[#666666] md:text-[15px]">Trusted by Company</p>
+                </div>
+                {/* <p className="text-center text-[13px] text-[#666666]">done</p> */}
+              </div>
+          </div>
+    </div>
+      {/* <div className="relative top-[12rem]">
         <p className="text-center text-3xl font-semibold">
           SUCCESS BY THE NUMBERS
         </p>
@@ -24,7 +57,7 @@ function Success() {
                 <p className="text-[#666666]">Trusted by Company</p>
             </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
